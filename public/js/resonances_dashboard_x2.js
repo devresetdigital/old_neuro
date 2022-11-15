42cdeb568e1adcae517322317a9498c1c2330f86
nicolas taglienti <nicotaglienti@gmail.com>
2022-11-15 10:57:03 -0300

    fix laurel road

--- public/js/resonances_dashboard_x2.js
+++ public/js/resonances_dashboard_x2.js
@@ -241,7 +241,7 @@ const loadSignalChart = (chartData, id ) => {
         let average =totalScore/count;
         let limitToShow = (average)+((maxValue-average)/4);
         for (let [name,value] of Object.entries(data)) {
-            if (parseFloat(value) >= limitToShow ){
+            if (data.length <=5 || parseFloat(value) >= limitToShow ){
                 keyToShow.push(name);
             }
         }
